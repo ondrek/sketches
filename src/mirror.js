a95e65810c549aff411ea29c263c6ada78591dc0
Samuel Ondrek <samuel@ondrek.com>
2016-07-26 18:45:49 +0200

    updated structure and creating corners

--- src/mirror.js
+++ src/mirror.js
@@ -18,17 +18,24 @@
     "use strict";
 
     var getBrowserSize = function(){
-        return { width: window.innerWidth || document.body.clientWidth, height: window.innerHeight || document.body.clientHeight }
-    }
-
-    var getCorners = function(){
         return {
-            tl:[0, 0],
-            tr:[getBrowserSize().x, 0],
-            bl:[0, getBrowserSize().y],
-            br:[getBrowserSize().x, getBrowserSize().y]
+            width: window.innerWidth || document.body.clientWidth,
+            height: window.innerHeight || document.body.clientHeight
         }
-    }
+    };
+
+    var getQuadrants = function(){
+        return [
+            [0, 0], // top left
+            [getBrowserSize().width/2, 0], // top center
+            [getBrowserSize().width, 0], // top right
+            [0, getBrowserSize().height/2], // middle left
+            [0, getBrowserSize().height], // bottom left
+            [getBrowserSize().width/2, getBrowserSize().height], // bottom center
+            [getBrowserSize().width, getBrowserSize().height], // bottom right
+            [getBrowserSize().width, getBrowserSize().height/2] // middle right
+        ]
+    };
 
 
     mirror.render = function(){
@@ -45,18 +52,16 @@
             mirror.ctx.stroke();
         };
 
-        draw(getCorners().tl);
-        draw(getCorners().tr);
-        draw(getCorners().bl);
-        draw(getCorners().br);
+        getQuadrants().forEach(function(el){
+            draw(el);
+        });
 
         mirror.ctx.strokeStyle="black";
         mirror.ctx.fillStyle = "black";
         mirror.ctx.fill();
         mirror.ctx.stroke();
 
-
-    }
+    };
 
 })();
 
@@ -67,14 +72,15 @@
     "use strict";
 
     window.requestAnimFrame = (function(){
-        return  window.requestAnimationFrame ||
+        return window.requestAnimationFrame ||
             window.webkitRequestAnimationFrame ||
             window.mozRequestAnimationFrame ||
             function(callback){ window.setTimeout(callback, 1000 / 60); };
     })();
 
     (function animationLoop(){
-        requestAnimFrame(animationLoop);
+        setTimeout(animationLoop, 400);
+        //requestAnimFrame(animationLoop);
         mirror.render();
     })();
 
